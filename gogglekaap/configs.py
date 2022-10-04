fa37c99e20dfcc62b0606f0499f287f0ddb26a00
jihunsim <sghkkk5777@hanmail.net>
2022-10-05 01:38:18 +0900

    221005

--- gogglekaap/configs.py
+++ gogglekaap/configs.py
@@ -1,3 +1,5 @@
+import os
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 class Config(object):
     """Flask Config"""
     SECRET_KEY = 'secretkey'
@@ -12,6 +14,11 @@ class DevelopmentConfig(Config):
     SEND_FILE_MAX_AGE_DEFAULT = 1 # 디버깅 모드에서 캐시 제거
     # TODO: Front호출시 처리
     WTF_CSRF_ENABLED = False # 디버깅 모드에서 CSRF 미적용
+    
+class TestingConfig(DevelopmentConfig):
+    __test__ = False
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_PATH, "sqlite_test.db")}'
     
 class ProductionConfig(DevelopmentConfig):
     pass
